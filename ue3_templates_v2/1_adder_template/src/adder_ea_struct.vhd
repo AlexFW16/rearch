b74a4ab2c443d46391feef1a6cbebd32feaa446b
AlexFW <apehers@gmail.com>
2023-04-19 13:23:07 +0200

    added 4-bit multiplexer, changed everything :)

--- ue3_templates_v2/1_adder_template/src/adder_ea_struct.vhd
+++ ue3_templates_v2/1_adder_template/src/adder_ea_struct.vhd
@@ -1,5 +1,6 @@
 
 -- We can just compute for every input bit the sum, with and without carriage with the full adder. Then we put it back together.
+--> NO, just make 2 carry ripple adders for 4 bit, and let them run parallel
 
 
 library IEEE; use IEEE.STD_LOGIC_1164.all;  -- must be added before each entity/arch.
@@ -16,7 +17,12 @@ architecture adder_arch of adder is
   carry, sum: out std_ulogic);
   end component;
 
-  component
+  component multiplexer
+   port(a, b: in std_ulogic_vector(4 downto 0);
+       control : in std_ulogic;
+       c : out std_ulogic_vector(4 downto 0));
+  end component;
+  
 
 
 begin
@@ -24,6 +30,68 @@ begin
   
 
 end adder_arch;
+
+
+-- Multplexer gets 2 2-bit inputs (a, b), where a0 is the sum and a1 is the carry of the prev. addition
+-- input a is the addition with carry, input b the addition without, hence a gets propagated if control = 1
+library IEEE; use IEEE.STD_LOGIC_1164.all;
+entity multiplexer is
+   port(a, b: in std_ulogic_vector(4 downto 0);
+       control : in std_ulogic;
+       c : out std_ulogic_vector(4 downto 0));
+end multiplexer;
+  
+library IEEE; use IEEE.STD_LOGIC_1164.all;
+architecture multiplexer_arch of multiplexer is
+begin 
+  multiplex_process : process(a, b, control) begin
+    if(control = '1') then
+      c <= a;
+    else
+      c <= b;
+    end if;
+  end process multiplex_process;
+end multiplexer_arch;
+
+
+-- 4-bit Adder
+library IEEE; use IEEE.STD_LOGIC_1164.all;
+entity adder_4bit is
+Port(a, b: in std_ulogic_vector(3, 0);
+    c_in : in std_ulogic;
+    sum : out std_ulogic(3, 0);
+    c_out: out std_ulogic);
+end adder_4bit;
+
+
+library IEEE; use IEEE.STD_LOGIC_1164.all;
+architecture adder_4bit_arch of adder_4bit is
+
+-- Full Adder component
+component full_adder
+Port(a, b, carry_in: in std_ulogic; -- input ports
+  sum, carry_out: out std_ulogic); -- output ports
+  end component;
+
+-- Intermediate carry signals
+signal c1, c2, c3 : std_ulogic;
+
+begin
+
+-- Port mapping
+-- Sets the first bit of the input of the 4bitadder as arguments for the full_adder (and output).
+-- Sets the carry of the first full_adder to the intermediate signal c1, which is used as input for the second full_adder.
+-- Uses the carriage input of the 4-bit adder as carriage input for the full_adder (useful for subtracting)
+fa1: full_adder port map(a(0), b(0), c_in, sum(0), c1);
+fa2: full_adder port map(a(1), b(1), c1, sum(1), c2);
+fa3: full_adder port map(a(2), b(2), c2, sum(2), c3);
+fa4: full_adder port map(a(3), b(3), c3, sum(3), c_out);
+
+end adder_4bit_arch;
+
+
+-- FROM SLIDES
+
 
 -- Full adder from slide 42
 library IEEE; use IEEE.STD_LOGIC_1164.all;
@@ -39,34 +107,13 @@ entity half_adder is
   port (in1,in2: in std_ulogic; carry: out std_ulogic; sum: out std_ulogic);
 end half_adder;
 
--- Multplexer gets 2 2-bit inputs (a, b), where a0 is the sum and a1 is the carry of the prev. addition
--- input a is the addition with carry, input b the addition without, hence a gets propagated if control = 1
-library IEEE; use IEEE.STD_LOGIC_1164.all;
-entity multiplexer is
-  port(a0, a1, b0, b1, control : in std_ulogic;
-       carry, sum : out std_ulogic);
-
-library IEEE; use IEEE.STD_LOGIC_1164.all;
-architecture multiplexer_arch of multiplexer is
-begin 
-  multiplex_process : process (a0, a1, b0, b1, control) begin
-    if(control = '0') then
-      sum <= a0; carry <= a1;
-    else
-      sum <= b0; carry <= b1;
-
-    end if;
-  end process multiplex_process;
-
-
-end multiplexer_arch;
-
 library IEEE; use IEEE.STD_LOGIC_1164.all;
 architecture structural of half_adder is
 begin
   carry <= in1 and in2;
   sum <= in1 xor in2;
 end structural;
+
 
 -- Behaviour of full_adder, from slides (48)
 architecture behavioral of full_adder is
